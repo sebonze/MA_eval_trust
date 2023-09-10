b8f64673aabcd5a7b76b57609d11c00220abb5c7
sebonze <sebonze.1414@gmx.de>
2023-09-10 22:55:10 +0200

    placeholder and skeleton

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import time
+import schnorr_main
+import pki_main
+import kerberos_main
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def prepare_trust_solutions():
+    """
+    This function prepares all Trust Solutions to run.
+    You can add any initialization or setup code here.
+    """
+    # Placeholder for preparation code
+    pass
+
+def measure_cycles(func):
+    """
+    Decorator to measure and print the number of processing cycles a function takes.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.process_time()
+        result = func(*args, **kwargs)
+        end_time = time.process_time()
+        cycles = end_time - start_time
+        print(f"{func.__name__} took {cycles} processing cycles.")
+        return result
+    return wrapper
+
+@measure_cycles
+def call_schnorr():
+    # Placeholder for schnorr_main callable function
+    schnorr_main.callable_function()
+
+@measure_cycles
+def call_pki():
+    # Placeholder for pki_main callable function
+    pki_main.callable_function()
+
+@measure_cycles
+def call_kerberos():
+    # Placeholder for kerberos_main callable function
+    kerberos_main.callable_function()
+
+if __name__ == "__main__":
+    prepare_trust_solutions()
+    call_schnorr()
+    call_pki()
+    call_kerberos()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
